6b0ab3b47dfa2bf936f8ab531752bfd899152a44
Niles <70601187+nilestate15@users.noreply.github.com>
2020-09-14 22:42:51 -0400

    Add files via upload
    Input Name

---
+++ HW4_Problem4.py
@@ -0,0 +1,9 @@
+#Problem 4
+#Input name
+Guestname = input("Input your first and last name: ")
+#prints input titlecase
+print(Guestname.title())
+#prints input uppercase
+print(Guestname.upper())
+#prints input lowercase
+print(Guestname.lower())
